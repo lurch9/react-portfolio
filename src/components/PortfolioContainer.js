c0b35858b8c106cfdb5b4cd7c2bca0a975ff2970
lurch <alexhann17@yahoo.com>
2022-02-26 12:01:19 -0800

    added navbar and mvp functionality

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+  
+
+    const renderPage = () => {
+      if (currentPage === 'Home') {
+        return <Home />;
+      }
+      else if (currentPage === 'About') {
+        return <About />;
+      }
+
+      else if (currentPage === 'Portfolio') {
+        return <Portfolio />;
+      }
+
+      else if (currentPage === 'Resume') {
+        return <Resume />;
+      }
+
+      return <Contact />;
+    };
+  
+  
+    return (
+      <div>
+        
+        <NavTabs currentPage={currentPage} handlePageChange={setCurrentPage} />
+        
+        {renderPage()}
+      </div>
+    );
+  }
---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Fusce non sem pellentesque erat euismod rhoncus in eu leo. 
+      In hac habitasse platea dictumst. Nam porttitor nulla eget metus lobortis fringilla. 
+      Vestibulum tempus quis lectus rhoncus congue. Phasellus varius ultrices ante eget cursus. 
+      Nunc eu velit justo. Integer ullamcorper quam aliquet tincidunt euismod. Vivamus nec semper tortor. 
+      Ut massa orci, tristique scelerisque eros ut, finibus mollis orci.
+      </p>
+    </div>
+  );
+}
